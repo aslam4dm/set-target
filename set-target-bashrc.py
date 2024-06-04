e73d1c4cdf82b9aacd55933b068ac1cca048f4b6
Aslam <35896884+aslam4dm@users.noreply.github.com>
2024-06-04 11:13:29 +0000

    Create set-target-bashrc.py
    Use this for bashrc

---
+++ set-target-bashrc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+def error():
+    print ('Usage: set-target <trgtdc/trgt1/trgt2/trgt3> <New Target IP>')
+    exit()
+
+try:
+    if sys.argv[1] and sys.argv[2]:
+        TRGT_REPLACE = sys.argv[1]
+        if TRGT_REPLACE not in ['trgtdc', 'trgt1', 'trgt2', 'trgt3']:
+            error()
+
+        TRGT_IP = sys.argv[2]
+
+        with open(f'/home/{os.getlogin()}/.bashrc') as f:
+            for l in f.readlines():
+                if TRGT_REPLACE in l and '=' in l:
+                    old_ip = l.split('=')[1].strip('\n')
+                    os.system(f"sudo sed -i 's/{TRGT_REPLACE}={old_ip}/{TRGT_REPLACE}={TRGT_IP}/g' /home/{os.getlogin()}/.bashrc")
+                    print ('{}: {} --> {}'.format(TRGT_REPLACE, old_ip,
+                            TRGT_IP))
+                    exit()
+    else:
+
+        error()
+        
+except IndexError:
+    error()
